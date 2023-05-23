26a0ef0842e37ee765793488069713e0f26dfada
Vpanwar-code <vivekpanwar183@gmail.com>
2023-05-24 00:33:47 +0530

    adding meals summary component and meals component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Fragment } from 'react';
 import Header from './components/Layout/Header';
-
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <div>
       <Fragment>
         <Header/>
+        <Meals/>
       </Fragment>
     </div>
   );
